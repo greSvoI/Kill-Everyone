78a1242180990f9793979a703c4071e6e4235f60
greSvoI <62199466+greSvoI@users.noreply.github.com>
2023-08-29 13:01:02 +0300

    Add camera state

--- Assets/RayCast.cs
+++ Assets/RayCast.cs
@@ -2,7 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class RayCast : MonoBehaviour
 {
-    
+	private void Update()
+	{
+		Debug.DrawRay(transform.position, transform.forward,Color.red,999f);
+	}
 }
